365176923d1d6cdbb370c8903b01324adf2ef237
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2020-12-07 11:11:36 -0500

    Update game1.js

--- game1.js
+++ game1.js
@@ -14,7 +14,8 @@ startingGame.addEventListener("click",startgame);
 if (game = true) {
   while (game = true) {
     
-    document.getElementById("game").style.backgroundImage = "href("https://e7.pngegg.com/pngimages/546/604/png-clipart-water-free-content-water-background-s-blue-angle.png")";
+    document.getElementById("game").backgroundImage = "url("https:/e7.pngegg.com/pngimages/546/604/png-clipart-water-free-content-water-background-s-blue-angle.png")";
     document.getElementById("game").innerHTML = "";
+    
   }
 }
