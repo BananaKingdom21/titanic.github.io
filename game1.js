bcc87491f2d0c3ac037c521b948c595c3b9a4ffe
BananaMaster21 <74437349+BananaMaster21@users.noreply.github.com>
2020-12-07 19:01:26 -0500

    Update game1.js

--- game1.js
+++ game1.js
@@ -1,25 +1,31 @@
-//epic variables
+/*epic variables*/
 
 var loopTimer = 1;
 var gameplay = 0;
 var buttonStart = document.getElementById("StartGameButton");
+var gameAreaJs = document.getElementById("gameArea");
 
-//game stuff
+/*game stuff*/
 
+//Where game starts.
 function StartTheGame () {
   game = 1;
 }
 
 buttonStart.addEventListener("click", StartTheGame);
 
+//Where game is working.
 function game () {
   
-  
+  gameAreaJs.innerHTML = "";
   
   
   requestAnimationFrame();
 }
+//Where game is executed.
 while (loopTimer > 0) {
+  
+  loopTimer = loopTimer + 1;
   
 if (game = 1) {
     game();
