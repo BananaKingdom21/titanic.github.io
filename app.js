c354301f9150da820bc0cc099dd5fe3d6e7db436
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2020-12-10 12:10:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*epic variables*/
 
-/*var loopTimer = 1;
+var loopTimer = 1;
 var gamePlay = 0;
 let button = document.querySelector("button");
 let gameAreajs = document.getElementById("gameArea");
 
 /*game stuff*/
-/*
+
 //Where game starts.
 function StartTheGame () {
   gamePlay = gamePlay + 1;
@@ -15,4 +15,4 @@ function StartTheGame () {
 button.click = function () {
   gameAreajs.innerHTML = "hello";
 }
-*/
+
