e0e962593effc5071d68871196297c3dafdd76f6
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2020-12-10 12:02:32 -0500

    Update and rename main.js to app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 /*epic variables*/
 
-var loopTimer = 1;
+/*var loopTimer = 1;
 var gamePlay = 0;
 let button = document.querySelector("button");
 let gameAreajs = document.getElementById("gameArea");
 
 /*game stuff*/
-
+/*
 //Where game starts.
 function StartTheGame () {
   gamePlay = gamePlay + 1;
 }
 
-button.onclick = function () {
+button.click = function () {
   gameAreajs.innerHTML = "hello";
 }
+*/
