edd0b0551a9ecef9dafdb66ec700551674cf8309
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2020-12-18 17:57:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 
-//game stuff
-
-var gameAreajs = document.getElementById("gameArea");
 var ice1 = document.getElementById("gameIce1");
 var playGame = 0;
 
 var button = document.querySelector("button");
 
 button.onclick = function () {
-  gameAreajs.innerHTML = "<canvas id=mycanvas width="400" height="400"></canvas>";
   playGame = 1;
 }
 
